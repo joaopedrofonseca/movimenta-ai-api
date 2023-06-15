84077a15a7dbdffae0e4d09c96eb91f99d7b85d7
joaopedrofonseca <joaopedrofonsec4@gmail.com>
2023-06-14 22:50:34 -0300

    fix: node_modules

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express, { Express } from 'express';
 import cors from 'cors';
-import { connectDb } from './config/database.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -8,11 +7,6 @@ const port = process.env.PORT || 4000;
 app
     .use(cors())
     .use(express.json())
-
-export function init(): Promise<Express> {
-    connectDb();
-    return Promise.resolve(app);
-}
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}.`);
